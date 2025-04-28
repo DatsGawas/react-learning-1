cf4814c463e7588f52b34e6e4842bde719fdf7d6
datsgawas <career.talktodats@gmail.com>
2025-04-28 22:31:35 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,29 +1,78 @@
-{/* <div id="parent">
-    <div id="child1">
-        <h1>I am H1 TAG</h1>
-        <h2>I am H2 Tag</h2>
-    </div>
-     <div id="child2">
-        <h1>I am H1 TAG</h1>
-        <h2>I am H2 Tag</h2>
-    </div>
-</div> */}
-
 // ReactElement (object) = HTML (Browser understands)
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-    "div", { id: "parent" },[
-    React.createElement("div", { id: "child1" }, [
-        React.createElement("h1", {}, "I am H1 Tag"),
-        React.createElement("h2", {}, "I am H2 Tag")
-    ]), React.createElement("div", { id: "child2" }, [
-        React.createElement("h1", {}, "I am H11 Tag"),
-        React.createElement("h2", {}, "I am H12 Tag")
-    ])
-]);
+/** 
+* Header
+* - Logo
+* - Nav Items
+* Body
+* - Search
+* - Restaurant COntainer
+* -  - Restaurant Card
+      - image
+      - Name
+      - rating
+      - cuisine
+      - delivery time
+      - cost for two
+* Footer
+* - Copyright
+* - Links
+* - Address
+* - C0ntact
+*/
+const Header = () => {
+     return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo"
+                src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png?nwm=1&nws=1&industry=fast-food&sf=&txt_keyword=All" />
+            </div>
+            <div className="nav-items"> 
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+     )
+}
 
-const heading = React.createElement("h1", { id: "heading" }, "Hello World from React!");
-console.log("parent", parent)
+const RestaurantCard = () => {
+    return (
+        <div className="res-card">
+            <img className="res-img" alt="" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/enj3srsnhbltbom2ovvh"/>
+            <h3>Meghana Foods</h3>
+            <h4>Biryani, Italian, Indian</h4>
+            <h4>4.4 Stars</h4>
+            <h4>38 mins</h4>
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <RestaurantCard />
+            </div>
+        </div>
+    )
+}
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Body />
+        </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<AppLayout />);
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-Hello React
+# Hello React
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR - Hot Module Replacement
+- File watching Algorithm - written in C++
+- Caching - faster builds
+- Image optimization
+- minifaction of files
+- bundling
+- compress
+- consistent hashing
+- code spliting
+- differential bundling - support older browser
+- Diagnostic
+- Error handling
+- HTTPS 
+- Tree shake - remove unwanted code
+- different build for dev and prod
